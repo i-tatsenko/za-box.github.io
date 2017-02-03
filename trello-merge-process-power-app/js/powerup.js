ec3e7e09fe8d15c476f07dd403d08257ce75b783
Ivan Tatsenko C5248476 <ivan.tatsenko@sap.com>
2017-02-03 18:14:13 +0200

    Trello power-up: show author in a badge

--- trello-merge-process-power-app/js/powerup.js
+++ trello-merge-process-power-app/js/powerup.js
@@ -6,46 +6,54 @@ function openJiraTask(url) {
 }
 
 function debugOptions(options) {
-    alert(JSON.stringify(options));
+    console.log(JSON.stringify(options));
 }
+
+function showAuthorBadge(trello) {
+    trello.card('members')
+        .then(result => {
+            console.log(result);
+            return [];
+        })
+}
+
 function openSettings(options) {
     debugOptions(options)
 }
 TrelloPowerUp.initialize({
-    'attachment-sections': function(t, options){
-        return[]
+    'attachment-sections': function (t, options) {
+        return []
     },
-    'attachment-thumbnail': function(t, options){
-            throw t.NotHandled();
+    'attachment-thumbnail': function (t, options) {
+        throw t.NotHandled();
     },
-    'board-buttons': function(t, options){
+    'board-buttons': function (t, options) {
         return [{
             icon: WHITE_ICON,
             text: 'Open Jira task',
             callback: () => openSettings(options)
         }];
     },
-    'card-badges': function(t, options){
+    'card-badges': function (t, options) {
         debugOptions(options);
         return [];
     },
-    'card-buttons': function(t, options) {
+    'card-buttons': function (t, options) {
         return [{
             icon: GRAY_ICON,
             text: 'Open Jira Task',
             callback: () => openJiraTask(options)
         }];
     },
-    'card-detail-badges': function(t, options) {
-        debugOptions(options);
-        return [];
+    'card-detail-badges': function (t, options) {
+        showAuthorBadge(t);
     },
-    'card-from-url': function(t, options) {
+    'card-from-url': function (t, options) {
         throw t.NotHandled();
 
     },
 
-    'show-settings': function(t, options){
+    'show-settings': function (t, options) {
         return t.popup({
             title: 'Settings',
             url: './settings.html',
